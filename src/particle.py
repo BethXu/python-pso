6d342829a1a8e0d6b2d1847e67f931018523fcea
Christien <pypmannetjies@gmail.com>
2017-03-27 21:32:59 +0200

    removed print statements

--- src/particle.py
+++ src/particle.py
@@ -33,20 +33,16 @@ class Particle(object):
         return self._velocity
 
     def update(self, inertia, nostalgia, envy, global_best):
-        # print self._velocity
         for i in range(self._dimensions):
-            # print i
             random_self = random.random()
-            # print random_self
             random_global = random.random()
-            # print random_global
             # Inertia
             self._velocity[i] = self._velocity[i] * inertia
             # Update own velocity according to memory of previous personal best position
             self._velocity[i] += nostalgia * random_self * (self._best_position[i] - self._position[i])
             # Update own velocity according to memory of global best position
             self._velocity[i] += envy * random_global * (global_best[i] - self._position[i])
+            # Update position with new velocity
             self._position[i] += self._velocity[i]
         self._update_best_position()
-        # print v
 
